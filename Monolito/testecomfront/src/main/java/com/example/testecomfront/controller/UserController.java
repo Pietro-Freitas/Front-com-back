50ff2f8c5c8f9d47e8dbe03d696226729cef1b23
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-14 11:52:33 -0300

    criação dos arquivos html e pagina de listar usuários

--- Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
+++ Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.example.testecomfront.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,4 +27,10 @@ public class UserController{
         return "redirect:/success.html";
     }
 
+    @GetMapping("/listar")
+    public String findAll(Model model){
+        List<User> users =  repository.findAll();
+        model.addAttribute("usuariosNoHtml", users);
+        return "usuarios";
+    }
 }
