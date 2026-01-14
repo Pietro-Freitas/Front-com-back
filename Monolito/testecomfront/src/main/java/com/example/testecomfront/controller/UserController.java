95f9d450d313816ff8789eece1fa57f41760378f
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-14 13:50:36 -0300

    melhora no front end

--- Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
+++ Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
@@ -17,40 +17,57 @@ import com.example.testecomfront.repository.UserRepository;
 @CrossOrigin(origins = "*")
 @Controller
 @RequestMapping("/user")
-public class UserController{
+public class UserController {
 
     @Autowired
     private UserRepository repository;
 
+    // Rota para abrir o formulário de login
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login"; // Procura templates/login.html
+    }
+
+    // Rota para abrir o formulário de registro
+    @GetMapping("/registrar")
+    public String registerPage() {
+        return "register"; // Procura templates/register.html
+    }
+
     @PostMapping("/registered")
-    public String register(User user){
+    public String register(User user) {
         repository.save(user);
         return "redirect:/success.html";
     }
 
     @GetMapping("/listar")
-    public String findAll(Model model){
-        List<User> users =  repository.findAll();
+    public String findAll(Model model) {
+        List<User> users = repository.findAll();
         model.addAttribute("usuariosNoHtml", users);
         return "usuarios";
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         return repository.findById(id).orElse(null);
     }
 
     @GetMapping("/deletar/{id}")
-    public String deleteById(@PathVariable("id") Long id){
+    public String deleteById(@PathVariable("id") Long id) {
         repository.deleteById(id);
         return "redirect:/user/listar";
     }
 
     @GetMapping("/editar/{id}")
-    public String editById(@PathVariable("id") Long id, Model model){
+    public String editById(@PathVariable("id") Long id, Model model) {
         User user = findById(id);
 
         model.addAttribute("usuarioParaEditar", user);
 
         return "editar";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
     }
 }
