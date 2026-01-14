ec8e3c8b8e486342c187f0b8f7f63e3045e563b2
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-14 12:54:18 -0300

    Criação da parte de editar usuário

--- Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
+++ Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
@@ -35,9 +35,22 @@ public class UserController{
         return "usuarios";
     }
 
+    public User findById(Long id){
+        return repository.findById(id).orElse(null);
+    }
+
     @GetMapping("/deletar/{id}")
     public String deleteById(@PathVariable("id") Long id){
         repository.deleteById(id);
         return "redirect:/user/listar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editById(@PathVariable("id") Long id, Model model){
+        User user = findById(id);
+
+        model.addAttribute("usuarioParaEditar", user);
+
+        return "editar";
     }
 }
