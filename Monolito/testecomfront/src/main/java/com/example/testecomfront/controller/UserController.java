bcb106155c55885fa5a31118010aceca82db3e27
Pietro Cardoso <pietrocardosof@gmail.com>
2026-01-14 12:17:34 -0300

    Delete adicionado

--- Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
+++ Monolito/testecomfront/src/main/java/com/example/testecomfront/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,5 +33,11 @@ public class UserController{
         List<User> users =  repository.findAll();
         model.addAttribute("usuariosNoHtml", users);
         return "usuarios";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deleteById(@PathVariable("id") Long id){
+        repository.deleteById(id);
+        return "redirect:/user/listar";
     }
 }
